dc43c17a59ccf47d6eca3ebb39f13b0053eed55b
kangmingtay <kang.ming1996@gmail.com>
2021-01-21 23:42:35 +0800

    create routes to post / get videos

---
+++ controller/video-controller.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kangmingtay/go-gin-proj/entity"
+	"github.com/kangmingtay/go-gin-proj/service"
+)
+
+type VideoController interface {
+	FindAll() []entity.Video
+	Save(ctx *gin.Context) entity.Video
+}
+
+type controller struct {
+	service service.VideoService
+}
+
+func New(service service.VideoService) VideoController {
+	return &controller{
+		service: service,
+	}
+}
+
+func (c *controller) FindAll() []entity.Video {
+	return c.service.FindAll()
+}
+
+func (c *controller) Save(ctx *gin.Context) entity.Video {
+	var video entity.Video
+	ctx.BindJSON(&video)
+	c.service.Save(video)
+
+	return video
+}
--- server.go
+++ server.go
@@ -1,14 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kangmingtay/go-gin-proj/controller"
+	"github.com/kangmingtay/go-gin-proj/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "OK!!",
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
+	})
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
 	})
 
 	server.Run(":8080")
